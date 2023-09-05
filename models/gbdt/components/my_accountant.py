8034dc9bfa83c9e01a43bc6ec65f65e272ae3289
Erchi Wang <erchiw@gmail.com>
2023-09-04 20:06:25 -0700

    change account and gbdt to support rdp hyper selection

--- models/gbdt/components/my_accountant.py
+++ models/gbdt/components/my_accountant.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 from autodp.mechanism_zoo import ExactGaussianMechanism, ExponentialMechanism, PureDP_Mechanism #, Renyi_hp_tune_Mechanism
-from autodp.calibrator_zoo import ana_gaussian_calibrator, generalized_eps_delta_calibrator
+from autodp.calibrator_zoo import ana_gaussian_calibrator
 from autodp.transformer_zoo import Composition
 from scipy.optimize import bisect
+
+from hyper_selection_Racheal import PrivateSelection
 
 
 class MyPrivacyAccountant():
@@ -14,7 +16,8 @@ class MyPrivacyAccountant():
                  num_trees, num_features, max_depth, split_method, task_type="classification", sketch_type="uniform", sketch_rounds=np.Inf,# Tree params
                  ratio_hist=None, ratio_selection=None, ratio_leaf=None, # budget ratio for different mechanism
                  selection_mechanism="exponential_mech",
-                 feature_interaction_method=None, feature_interaction_k=None
+                 feature_interaction_method=None, feature_interaction_k=None,
+                 hyper_tnb_eta=None, hyper_tnb_gamma=None
                  ):
 
         # sanity check and bounds for CE loss and sigmoid least square
@@ -81,6 +84,11 @@ class MyPrivacyAccountant():
 
         if split_method == "hyper_tune":
             self.hyper_sensitivity = np.sqrt(self.grad_sensitivity**2 + self.hess_sensitivity**2)
+            self.dist_dict = {"eta": hyper_tnb_eta, "gamma": hyper_tnb_gamma}
+            self.dist = "TNB" 
+        else:
+            self.dist = None
+            self.dist_dict = None
 
         self.loss_name = loss_name
         self.sketch_type = sketch_type
@@ -102,7 +110,7 @@ class MyPrivacyAccountant():
             raise ValueError("split_method didn't implemented")
         
 
-    def create_tree_mechanism(self, gau_sigma_hist, selection_eps, gau_sigma_leaf):
+    def create_tree_mechanism(self, gau_sigma_hist, selection_eps, gau_sigma_leaf, gau_sigma_selection=None, dist_dic=None):
         compose = Composition()
 
         if self.split_method == "grad_based":
@@ -111,7 +119,10 @@ class MyPrivacyAccountant():
             elif self.selection_mechanism == "permutate_flip":
                 selection_node = PureDP_Mechanism(selection_eps, name="permute_flip_node")
         elif self.split_method == "hyper_tune":
-            selection_node = PureDP_Mechanism(selection_eps, name="hyper_selection") # non-optimal, change later
+            #selection_node = PureDP_Mechanism(selection_eps, name="hyper_selection") # non-optimal, change later
+            p_select = PrivateSelection()
+            node_base_mech = ExactGaussianMechanism(gau_sigma_selection)
+            selection_node = p_select(node_base_mech, dist="TNB", dist_params={"eta":dist_dic["eta"], "gamma":dist_dic["gamma"]})
         else:
             raise NotImplementedError("sk not imp")
         
@@ -157,14 +168,28 @@ class MyPrivacyAccountant():
         eps_per_leaf = total_design_eps * self.ratio_per_leaf
         sigma_leaf = self.get_gau_sigma(eps_per_leaf, delta)
         
+        if self.split_method == "hyper_tune":
+            sigma_selection = self.get_gau_sigma(eps_per_selection, delta)
+        else:
+            sigma_selection = None
+            
         if self.sketch_type == "adaptive_hessian":
             eps_per_hist = total_design_eps * self.ratio_per_hist
             sigma_hist = self.get_gau_sigma(eps_per_hist, delta)
-
-            tree_mech = self.create_tree_mechanism(sigma_hist, eps_per_selection, sigma_leaf)    
+            tree_mech = self.create_tree_mechanism(gau_sigma_hist=sigma_hist, 
+                                                   selection_eps=eps_per_selection, 
+                                                   gau_sigma_leaf=sigma_leaf, 
+                                                   gau_sigma_selection=sigma_selection,
+                                                   dist_dic=self.dist_dict)    
             actual_eps = tree_mech.get_approxDP(self.delta)
+        
         elif self.sketch_type == "uniform":
-            tree_mech = self.create_tree_mechanism(0, eps_per_selection, sigma_leaf)    
+            tree_mech = self.create_tree_mechanism(gau_sigma_hist=0, 
+                                                   selection_eps=eps_per_selection, 
+                                                   gau_sigma_leaf=sigma_leaf, 
+                                                   gau_sigma_selection=sigma_selection,
+                                                   dist_dic=self.dist_dict)    
+        
             actual_eps = tree_mech.get_approxDP(self.delta)
 
         return actual_eps
@@ -234,7 +259,8 @@ class MyPrivacyAccountant():
         elif mech_type == "hist":
             sigma = self.get_gau_sigma(self.calibrated_epsilon * self.ratio_per_hist, self.delta)
         elif mech_type == "renyi_hyper": # this is for releasing AdaSSP style score for splititing selection 
-            sigma = self.get_gau_sigma(self.calibrated_epsilon * self.ratio_per_selection/2, self.delta) ## TODO: Renyi_hp
+            #sigma = self.get_gau_sigma(self.calibrated_epsilon * self.ratio_per_selection/2, self.delta) ## TODO: Renyi_hp
+            sigma = self.get_gau_sigma(self.calibrated_epsilon * self.ratio_per_selection, self.delta) 
         return sigma
     
 
